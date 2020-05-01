8c7362cef6caacd04a18394457b01d2460646b08
Duy <huynhduytam2210@gmail.com>
2020-05-01 17:10:55 +0700

    Display Channel Info in MetaPanel

--- src/components/MetaPanel/MetaPanel.js
+++ src/components/MetaPanel/MetaPanel.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { Segment, Header, Accordion, Icon } from 'semantic-ui-react'
+import { Segment, Header, Accordion, Icon, Image } from 'semantic-ui-react'
 
 class MetaPanel extends React.Component {
     state = {
+        channel: this.props.currentChannel,
         privateChannel: this.props.isPrivateChannel,
         activeIndex: 0
     }
@@ -15,13 +16,13 @@ class MetaPanel extends React.Component {
     }
 
     render() {
-        const { activeIndex, privateChannel } = this.state
-        if(privateChannel) return null
+        const { activeIndex, privateChannel, channel } = this.state
+        if (privateChannel || !channel) return null
 
         return (
-            <Segment>
+            <Segment loading={!channel}>
                 <Header as='h3' attached='top'>
-                    About # Channel
+                    About # {channel && channel.name}
                 </Header>
                 <Accordion styled attached='true'>
                     <Accordion.Title
@@ -34,7 +35,7 @@ class MetaPanel extends React.Component {
                         Channel Details
                     </Accordion.Title>
                     <Accordion.Content active={activeIndex === 0}>
-                        details
+                        {channel && channel.details}
                     </Accordion.Content>
 
                     <Accordion.Title
@@ -60,7 +61,10 @@ class MetaPanel extends React.Component {
                         Created By
                     </Accordion.Title>
                     <Accordion.Content active={activeIndex === 2}>
-                        creator
+                        <Header as ='h3'>
+                            <Image circular src={channel && channel.createdBy.avatar} />
+                            {channel && channel.createdBy.name}
+                        </Header>
                     </Accordion.Content>
                 </Accordion>
             </Segment>
